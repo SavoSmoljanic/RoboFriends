7551a64f88f3794f0ea39a98a061fabb1c978d88
Savo Smoljanic <salvation123123@gmail.com>
2022-08-22 09:30:42 +1000

    Updates

---
+++ precache-manifest.35d1f11b4fed0d1fbaf470b1b735373a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb13ab573fd672f48d0b",
+    "url": "/github.io/SavoSmoljanic/RoboFriends/static/css/main.d5243aea.chunk.css"
+  },
+  {
+    "revision": "cb13ab573fd672f48d0b",
+    "url": "/github.io/SavoSmoljanic/RoboFriends/static/js/main.cb13ab57.chunk.js"
+  },
+  {
+    "revision": "0e9167439b236bcc126b",
+    "url": "/github.io/SavoSmoljanic/RoboFriends/static/css/1.94204a19.chunk.css"
+  },
+  {
+    "revision": "0e9167439b236bcc126b",
+    "url": "/github.io/SavoSmoljanic/RoboFriends/static/js/1.0e916743.chunk.js"
+  },
+  {
+    "revision": "3007853389b23876c101",
+    "url": "/github.io/SavoSmoljanic/RoboFriends/static/js/runtime~main.30078533.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/github.io/SavoSmoljanic/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fa94cae0af44e40714df7a899b953913",
+    "url": "/github.io/SavoSmoljanic/RoboFriends/index.html"
+  }
+];
